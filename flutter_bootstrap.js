19812c9ee8864f1b2753855c99ee7fa24c944692
bhcpneus <bhcpneus@gmail.com>
2026-04-26 10:27:06 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "665199858" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3414257175" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
